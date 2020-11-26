dcf96f84d2b18b566b99fa1b3f7239bb46b78d36
Sergio Angulo <sergio.angulo.c@gmail.com>
2020-11-26 17:42:01 -0300

    Update data_analisis.py
    Se elomiona funcion con error

--- data_analisis.py
+++ data_analisis.py
@@ -93,14 +93,10 @@ def showScatterPlots(df):
     plt.ylabel(f"[{b}]")
     #plt.title('')
     plt.show()
-
-def lin_reg(x): # Una posible mejora, es que reciba los betas por parametros de la función
-    y_hat = (beta_1 * x) + beta_0 #Aplicamos la forma de la regresión lineal 
-    return y_hat
     
 def getMetricasErrorRegresion(y, y_tongo, name):
   mape = np.sum(np.abs((y - y_tongo)/y)) / len(y)
-  mse = mean_squared_error(y , lin_reg(y_tongo))
+  mse = mean_squared_error(y , y_tongo)
   mae = mean_absolute_error(y , y_tongo)
   r2 = r2_score(y,y_tongo)
   return pd.DataFrame([mape,mse,mae,r2], columns=[name], index=["mape", "mse","mae","r2_score"])
